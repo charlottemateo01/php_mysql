60992b9b393df453160106cf6068d2e9e4f32d1d
CharlotteMateo <101971085+charlottemateo01@users.noreply.github.com>
2022-04-01 01:30:10 +0800

    Add files via upload

--- 202A_Mateo_Charlotte/index.php
+++ 202A_Mateo_Charlotte/index.php
@@ -1,49 +1,53 @@
+<?php
+include_once('connection.php');
+$sql = "SELECT * from songs";
+$result = mysqli_query($conn, $sql);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Top 5 Favorite Songs</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Mateo Top Songs</title>
 </head>
 <body>
-    <h1>My Top 5 Favorite Songs</h1>
-    <a href="add.php"><br>
-    <button id="myButton" class="float-left submit-button" style="background-color: #4CAF50;" >Add New Songs</button><br>
-   </a>
-   <br>
-    <table table border = '1' cellspacing = '0' cellpadding='10' style="text-align: center;">
-        <thead>
-            <tr>
-                <th>Title</th>
-                <th>Artist</th>
-                <th>Composer</th>
-                <th>Album</th>
-                <th>Released Date</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php 
-              include 'db_connection.php';
+<div class="container card mt-5">
 
-              $sql = "SELECT * from songs";
-              $result = mysqli_query($conn, $sql);
-
-              if (mysqli_num_rows($result) > 0) {
-                  while ($row = mysqli_fetch_assoc($result)) {
-                      echo "<tr>";
-                      echo "<td>" . $row['title'] . "</td>";
-                      echo "<td>" . $row['artist'] . "</td>";
-                      echo "<td>" . $row['composer'] . "</td>";
-                      echo "<td>" . $row['album'] . "</td>";
-                      echo "<td>" . $row['released_date'] . "</td>";
-                      echo "</tr>";
-                  }
-              } else {
-                  echo "<tr><td colspan='5'>No results found!</td></tr>";
-              }
-            ?>
-        </tbody>
-    <table>
+  <div class ="col-md-13 ">
+  <h2>Top Favorite Songs of Charlotte Mateo</h2><br>
+  <a href="create.php" class="btn btn-outline-primary float-left">Add New Songs</a>
+  </div>
+  <table class="table table-bordered mt-2">
+    <thead>
+      <tr>
+        <th>id</th>
+        <th>Title</th>
+        <th>Artist</th>
+        <th>Composer</th>
+        <th>Released Date</th>
+        <th>Album</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+    <tbody>
+    <?php while ($row = mysqli_fetch_assoc($result)) {?>
+           <tr>
+             <td><?=$row['id']?></td>
+             <td> <?=$row['title']?>  </td>
+             <td>  <?=$row['artist']?>  </td>
+             <td>  <?=$row['composer']?> </td>
+             <td>  <?=$row['released_date']?>  </td>
+             <td>  <?=$row['album']?>  </td>
+             <td><a href="controller.php?id=<?php echo $row['id']; ?>">Delete</a></td>
+           </tr>
+    <?php } ?>
+    </tbody>
+  </table>
+</div>
 </body>
 </html>
